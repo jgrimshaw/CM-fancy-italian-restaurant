f14d9fa18cfb4c34973be99c91a28538786a2939
jgrimshaw <muchstudio@hotmail.com>
2021-02-08 18:55:39 -0700

    sort and render menuItems in their categories

--- src/index.js
+++ src/index.js
@@ -4,4 +4,48 @@ import menu from "./menu.js";
 
 const menuItems = menu.items;
 
+
+// sort by menuOrder
+const sortItems = () => {
+    menuItems.sort((a, b) => {
+        return a.menuOrder - b.menuOrder;
+    })
+}
+
+
+// render items
+const renderItems = () => menuItems.forEach(item => {
+    
+    const starters = document.querySelector('#starters');
+    const pasta = document.querySelector('#pasta');
+    const pizza = document.querySelector('#pizza');
+    const name = document.createElement('h3');
+    const description = document.createElement('p');
+
+    if (item.type === 'starters') {
+        name.textContent = item.name
+        description.textContent = item.description
+        starters.append(name)
+        starters.append(description)
+    } else if (item.type === 'pasta') {
+        name.textContent = item.name
+        description.textContent = item.description
+        pasta.append(name)
+        pasta.append(description)
+    } else if (item.type === 'pizza') {
+        name.textContent = item.name
+        description.textContent = item.description
+        pizza.append(name)
+        pizza.append(description)
+    }
+})
+
+
+const renderApp = () => {
+    sortItems()
+    renderItems(menuItems)
+}
+
+
 console.log(menuItems);
+renderApp();
