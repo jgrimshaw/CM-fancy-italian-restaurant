6519b98f6fe7e08e237b2c3d2a56ccb373a9a2a7
jgrimshaw <muchstudio@hotmail.com>
2021-02-09 22:43:29 -0700

    toggle spicy options

--- src/index.js
+++ src/index.js
@@ -12,18 +12,22 @@ const sortItems = () => {
     })
 }
 
+
+
 // categorize, check if spicy, render items
 const renderItems = () => menuItems.forEach(item => {
-    
+
     const starters = document.querySelector('#starters');
     const pasta = document.querySelector('#pasta');
     const pizza = document.querySelector('#pizza');
 
+    const itemContainer = document.createElement('div')
+    itemContainer.classList.add('item-container');
     const name = document.createElement('h3');
     const description = document.createElement('p');
     const price = document.createElement('p');
     const spicy = document.createElement('span');
-    spicy.classList.add('spice')
+    spicy.classList.add('spicy')
 
     name.textContent = item.name
     description.textContent = item.description
@@ -32,22 +36,36 @@ const renderItems = () => menuItems.forEach(item => {
 
     item.spicy ? name.append(spicy) : null;
 
-    if (item.type === 'starters') {
-        starters.append(name, description, price)
-    } else if (item.type === 'pasta') {
-        pasta.append(name, description, price)
-    } else if (item.type === 'pizza') {
-        pizza.append(name, description, price)
+    if(item.spicy) {
+        itemContainer.classList.add('spicy');
     }
+    
+    if (item.type === 'starters') {
+        itemContainer.append(name, description, price)
+        starters.append(itemContainer)
+    } else if (item.type === 'pasta') {
+        itemContainer.append(name, description, price)
+        pasta.append(itemContainer)
+    } else if (item.type === 'pizza') {
+        itemContainer.append(name, description, price)
+        pizza.append(itemContainer)
+    }
+
 })
 
-
+document.querySelector('#check').onchange = function( ) {
+    menuItems.forEach(item => {
+        if(item.spicy) {
+            console.log(item)
+            document.querySelector('.spicy').parentNode.style.display = this.checked ? 'block' : 'none'
+        }
+    })
+};
 
 const renderApp = () => {
     sortItems()
     renderItems()
 }
-
 
 console.log(menuItems);
 renderApp();
